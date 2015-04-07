a90ef89b4c872902fffd67a3c36457d8429b7b1c
shazra <sayan.mnit@gmail.com>
2015-04-07 11:32:01 +0200

    Added functionality for xml parsing, returning objects and fine tuning the test cases.

--- lib/mondrian/cube_client/connection.rb
+++ lib/mondrian/cube_client/connection.rb
@@ -1,127 +1,70 @@
 require 'net/http'
+require 'nokogiri'
+require_relative "XmlParser"
 
 module Mondrian
   module CubeClient
     class Connection
 
-      attr_reader :base_url
+      attr_reader :base_url, :list_objs
 
       def initialize(base_url)
         @base_url=base_url
         @url=URI.parse(base_url)
       end
 
-      def get(catalog_name,cube_name)
-        url="#{base_url}/cube/#{catalog_name}/#{cube_name}"
-        xml=Net::HTTP.get_response(URI.parse(url))
-        cube=parse_response(xml)
-        cube.connection=self
-        cube
+      def get(catalog_name, cube_name)
+        puts "catalog_name = #{catalog_name} and cube_name = #{cube_name}"
+        url = ""
+        if ((catalog_name.nil? || catalog_name.empty?) && (cube_name.nil? || cube_name.empty?))
+          url = "#{base_url}/cubes"          
+        elsif ((catalog_name.nil? || catalog_name.empty?) && !(cube_name.nil? || cube_name.empty?))
+          url = "#{base_url}/cube/#{cube_name}"          
+        else          
+          url = "#{base_url}/cube/#{catalog_name}/#{cube_name}"
+        end
+
+        resp = Net::HTTP.get_response(URI.parse(url)) 
+              
+        response_xml = resp.body.strip
+        parse_response(response_xml) 
+        response_xml  
       end
 
-      def create(catalog_name,cube_name,xml)
+      def create(catalog_name,cube_name, xml)
         put_path="#{@url.path}/putcube/#{catalog_name}/#{cube_name}"
         req = Net::HTTP::Put.new(put_path, initheader = { 'Content-Type' => 'text/plain'})
         req.body = xml
-        Net::HTTP.new(@url.host, @url.port).start {|http| http.request(req) }
+        resp = Net::HTTP.new(@url.host, @url.port).start {|http| http.request(req) }
+        resp.body.strip
       end
 
-      def list(catalog_name)
-
-      end
-
-
-      def update
-
-      end
+      def update(catalog_name, cube_name, xml)
+        put_path="#{@url.path}/putcube/#{catalog_name}/#{cube_name}"
+        req = Net::HTTP::Put.new(put_path, initheader = { 'Content-Type' => 'text/plain'})
+        req.body = xml
+        resp = Net::HTTP.new(@url.host, @url.port).start {|http| http.request(req) }        
+        resp.body.strip
+      end  
 
       def delete(catalog_name,cube_name)
+        del_path = "#{@url.path}/deletecube/#{catalog_name}/#{cube_name}"
+        http = Net::HTTP.new(@url.host, @url.port)    
+        resp = http.send_request('DELETE', del_path) 
+        resp.body.strip
+      end
 
+      def parse_response(response_xml)
+        puts "The response_xml is #{response_xml}"
+        puts ""
+        myparser = MyParser.new()
+        parser = Nokogiri::XML::SAX::Parser.new(myparser)         
+        parser.parse(response_xml)
+        puts "Inspecting = #{parser.inspect}"
+        @list_objs = myparser.list_catalogs
+        @list_objs.push(response_xml)
+        puts "the array value for #{@list_objs}"        
       end
     end
-
   end
 end
-
-# begin
-#
-#   cube_name = 'Warehouse1'
-#   catalog_name = 'FoodMart'
-# #get all the cubes
-# #url = 'http://localhost:8080/mondrian/cubecrudapi/cubes'
-#
-# #get a cube based on a catalog
-#
-# =begin
-#
-# url = 'http://localhost:8080/mondrian/cubecrudapi/cube/' + catalog_name + '/' + cube_name
-#
-#
-# #get a catalog definition
-#
-# 	url = 'http://localhost:8080/mondrian/cubecrudapi/catalog/' + catalog_name
-#
-#
-#
-# =end
-# #put a catalog/cube definition
-#
-# # Read the input xml from a file
-#
-# #url = 'http://localhost:8080/mondrian/cubecrudapi/putcube/' + catalog_name + '/' + cube_name
-#
-#   url = '/mondrian/cubecrudapi/putcube/' + catalog_name + '/' + cube_name
-#
-#
-#   port = 8080
-#   host = "localhost"
-#   path = '/mondrian/cubecrudapi/putcube/' + catalog_name + '/' + cube_name
-#
-#
-# #url = '/mondrian/cubecrudapi/deletecube/' + catalog_name + '/' + cube_name
-#
-#   file = File.new("/home/shazra/work/Docs/Project mondrian/mondrianxmlas/inputPUT.xml", "r")
-#   input_xml = ""
-#   while (line = file.gets)
-#     input_xml = input_xml + line
-#     puts line
-#   end
-#   file.close
-#   puts "The url used is=" + url
-#
-#   req = Net::HTTP::Put.new(path, initheader = {'Content-Type' => 'text/plain'})
-#   req.body = input_xml
-#   response = Net::HTTP.new(host, port).start { |http| http.request(req) }
-#   puts response.body
-#
-#   input_xml = input_xml.gsub! '"', '\"'
-#   puts "----Input xml-----"
-#   puts input_xml
-#
-#
-#   puts "------------The response xml from Mondrian--------------------"
-# #http = Net::HTTP.new('localhost', 8080)
-#
-# #resp = http.send_request('PUT', url, input_xml)
-#
-#   request = Net::HTTP::Put.new(url)
-#
-# #resp = http.send_request('DELETE', url )
-#
-# #resp = Net::HTTP.get_response(URI.parse(url))
-#   case resp
-#     when Net::HTTPSuccess then
-#       puts resp.body
-#     when Net::HTTPRedirection then
-#       location = resp['location']
-#       warn "redirected to #{location}"
-#     else
-#       puts resp.value
-#   end
-#
-# rescue Exception => e
-#   puts e.message
-#   puts e.backtrace.inspect
-# end
-#
-# puts "----The End!!----"
--- spec/lib/mondiran/cube_client_spec.rb
+++ spec/lib/mondiran/cube_client_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+RSPEC_TEST_URL='http://localhost:8080'
 
 RSpec.describe Mondrian::CubeClient do
 
@@ -6,34 +7,47 @@ RSpec.describe Mondrian::CubeClient do
     File.read(RSPEC_APP_PATH.join('fixtures','cube_definition.xml'))
   }
   let(:connection) {
-    Mondrian::CubeClient.connection("http://192.168.178.104:8080/mondrian/cubecrudapi")
+    Mondrian::CubeClient.connection( RSPEC_TEST_URL + "/mondrian/cubecrudapi")
   }
-  it "sets the host and port for a connection" do
-    expect(connection.base_url).to eq("http://192.168.178.104:8080/mondrian/cubecrudapi")
+  
+    it "sets the host and port for a connection" do
+      expect(connection.base_url).to eq( RSPEC_TEST_URL + "/mondrian/cubecrudapi")
+    end
+
+  describe 'show cube for given catalog name' do
+    it "gets a cube's definition" do
+      expect(connection.get('mycat', 'mycube')).not_to be_empty
+    end
   end
 
-  describe 'show cube' do
+  describe 'show cube for given cube name' do
     it "gets a cube's definition" do
-      expect(connection.get('mycat', 'mycube')).to eq("")
+      expect(connection.get('', 'mycube')).not_to be_empty
+    end
+  end
+
+  describe 'list all cubes' do
+    it "gets all cubes with definitions" do
+      expect(connection.get('', '')).not_to be_empty
     end
   end
 
   describe 'update cube' do
-
+    it "updates a cube's definition" do
+      puts "testing ::#{connection.update('mycat', 'mycube', cubedefinition).inspect}"
+      expect(connection.update('mycat', 'mycube', cubedefinition)).not_to be_empty
+    end
   end
 
   describe 'create cube' do
     it "creates a cube" do
-      expect(connection.create('mycat','mycube',cubedefinition)).to eq("")
+      expect(connection.create('mycat','mycube',cubedefinition)).not_to be_empty
     end
   end
 
   describe 'delete cube' do
-
+    it "deletes a cube" do
+      expect(connection.delete('mycat','mycube')).not_to be_empty
+    end
   end
-
-  describe 'list cubes in catalog' do
-
-  end
-
 end
